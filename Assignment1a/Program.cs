00c9ff861680b52a968aef756ffe49234a42e46e
Jacob Misirian <misirianrule@gmail.com>
2015-08-31 10:13:12 -0500

    Added Assignment 1a

---
+++ Assignment1a/Program.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Assignment1A
+{
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+			DisplayInfo();
+		}
+
+		public static void DisplayInfo()
+		{
+			Console.WriteLine("Jacob Misirian");
+			Console.WriteLine("ITDEV 110");
+			Console.WriteLine("Bob Menzl");
+			Console.WriteLine(DateTime.Now);
+		}
+	}
+}
