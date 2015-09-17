8b8dd0c184745f4f1ac89b020057e6bc50bce803
Jacob Misirian <misirianrule@gmail.com>
2015-09-17 10:11:53 -0500

    Added Farenheit To Celsius

---
+++ FtoC/FtoC/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FtoC
+{
+    class Program
+    {
+        private static double farenheit { get; set; }
+
+        static void Main(string[] args)
+        {
+            farenheit = prompt();
+            displayCelsius(calculate());
+
+            Console.ReadKey();
+        }
+
+        private static double prompt()
+        {
+            Console.Write("Enter a temperature in farenheit to convert to celsius: ");
+            return Convert.ToDouble(Console.ReadLine());
+        }
+
+        private static double calculate()
+        {
+            return (farenheit - 32) / 1.8;
+        }
+
+        private static void displayCelsius(double celsius)
+        {
+            Console.WriteLine("Original was: " + farenheit);
+            Console.WriteLine("In Celsius: " + celsius.ToString("N1") + "°C");
+        }
+    }
+}
