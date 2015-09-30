5b666ba080711cbcaa1e18c72c8b9d7adffd0f86
Jacob Misirian <misirianrule@gmail.com>
2015-09-30 16:35:25 -0500

    Added Assignment 3

---
+++ Assignment3/Assignment3/Info.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    public class Info
+    {
+        private string assignmentName;
+
+        public Info(string assignmentName)
+        {
+            this.assignmentName = assignmentName;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Name:\tJacob Misirian");
+            Console.WriteLine("Class:\tITDEV 110");
+            Console.WriteLine("Teacher:\tBob Menzl");
+            Console.WriteLine("Assignment:\t" + this.assignmentName);
+            Console.WriteLine("Date:\t" + DateTime.Today.ToString("d") + "\n\n");
+        }
+    }
+}
---
+++ Assignment3/Assignment3/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Info("Assignment3").DisplayInfo();
+
+            Console.WriteLine(add(3, 2)); // Expected 5
+            Console.WriteLine(subtract(6, 4, 1)); // Expected 1
+            Console.WriteLine(multiply(4, 5, 2, 3)); // Expected 120
+            Console.WriteLine(divide(30, 2, 5)); // Expected 3
+            Console.WriteLine(modulus(40, 3)); // Expected 1
+
+            Console.ReadKey();
+        }
+
+        private static double add(params double[] args)
+        {
+            double total = args[0];
+            for (int x = 1; x < args.Length; x++)
+                total += args[x];
+
+            return total;
+        }
+        private static double subtract(params double[] args)
+        {
+            double total = args[0];
+            for (int x = 1; x < args.Length; x++)
+                total -= args[x];
+
+            return total;
+        }
+        private static double multiply(params double[] args)
+        {
+            double total = args[0];
+            for (int x = 1; x < args.Length; x++)
+                total *= args[x];
+
+            return total;
+        }
+        private static double divide(params double[] args)
+        {
+            double total = args[0];
+            for (int x = 1; x < args.Length; x++)
+                total /= args[x];
+
+            return total;
+        }
+        private static double modulus(params double[] args)
+        {
+            double total = args[0];
+            for (int x = 1; x < args.Length; x++)
+                total %= args[x];
+
+            return total;
+        }
+    }
+}
