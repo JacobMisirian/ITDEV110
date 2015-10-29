8cfe8b212304dec8259f3df26b4068c2d90c7ba2
Jacob Misirian <misirianrule@gmail.com>
2015-10-29 09:08:07 -0500

    Added ExtraCredit2

---
+++ ExtraCredit2/ExtraCredit2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtraCredit2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations operations = new MathOperations();
+            UserInterface ui = new UserInterface();
+
+            switch (ui.DisplayMenu("Circle", "Rectangle", "Surface of Cylinder"))
+            {
+                case 0:
+                    Console.WriteLine("Area of circle is: " + operations.AreaOfCircle(ui.PromptForDouble("Radius: ")));
+                    break;
+                case 1:
+                    Console.WriteLine("Area of rectangle is: " + operations.AreaOfRectangle(ui.PromptForDouble("Length: "), ui.PromptForDouble("Width: ")));
+                    break;
+                case 2:
+                    Console.WriteLine("Surface area of cylinder is: " + operations.SurfaceAreaOfCylinder(ui.PromptForDouble("Radius: "), ui.PromptForDouble("Height: ")));
+                    break;
+                default:
+                    throw new Exception("Something has gone terribly wrong here.");
+            }
+
+            Console.Read();
+        }
+    }
+}
