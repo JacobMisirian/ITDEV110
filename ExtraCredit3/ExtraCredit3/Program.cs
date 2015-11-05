109f2bd2c45fdd0d4963362eb836e116ff33815e
Jacob Misirian <misirianrule@gmail.com>
2015-11-05 10:36:33 -0600

    Added ExtraCredit3

---
+++ ExtraCredit3/ExtraCredit3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExtraCredit3
+{
+    class Program
+    {
+        private static Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            int lowest = -1;
+            int highest = -1;
+            int oddCounter = 0;
+
+            for (int x = 0; x < 1000; x++)
+            {
+                int number = random.Next(0, 100001);
+                if (number % 2 != 0)
+                    oddCounter++;
+                if (lowest == -1 || lowest > number)
+                    lowest = number;
+                if (highest < number)
+                    highest = number;
+            }
+
+            MessageBox.Show("Highest: " + highest + ".Lowest: " + lowest + ". Number of odd numbers: " + oddCounter);
+        }
+    }
+}
